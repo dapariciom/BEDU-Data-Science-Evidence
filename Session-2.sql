0b7fd3781a66771a992eba4d0ce77f6bd01799e8
Daniel Aparicio <dapariciom@outlook.com>
2020-11-03 22:56:14 -0600

    Session-2

--- Session-2.sql
+++ Session-2.sql
@@ -1,7 +1,13 @@
 USE tienda;
+USE classicmodels;
 SHOW TABLES;
+DESCRIBE empleado;
 DESCRIBE puesto;
 DESCRIBE venta;
+DESCRIBE employees;
+DESCRIBE products;
+DESCRIBE orderdetails;
+DESCRIBE orders;
 
 # RETO 1
 # ¿Qué artículos incluyen la palabra Pasta en su nombre?
@@ -34,3 +40,41 @@ SELECT nombre, sum(salario) FROM puesto GROUP BY nombre;
 SELECT id_empleado, count(*) FROM venta GROUP BY id_empleado;
 # ¿Cuál es el número total de ventas por artículo?
 SELECT id_articulo, count(*) FROM venta GROUP BY id_articulo;
+
+# Ejercicios
+# Dentro de la tabla employees, obten el número de empleado, apellido y nombre de todos los empleados cuyo nombre empiece con a
+SELECT employeeNumber, lastName, firstName FROM employees WHERE firstName LIKE'a%';
+# Dentro de la tabla employees, obten el número de empleado, apellido y nombre de todos los empleados cuyo nombre termina con on
+SELECT employeeNumber, lastName, firstName FROM employees WHERE firstName LIKE '%on';
+# Dentro de la tabla employees, obten el número de empleado, apellido y nombre de todos los empleados cuyo nombre incluye la cadena on
+SELECT employeeNumber, lastName, firstName FROM employees WHERE firstName LIKE '%on%';
+# Dentro de la tabla employees, obten el número de empleado, apellido y nombre de todos los empleados cuyos nombres tienen tres letras e inician con T y finalizan con m
+SELECT employeeNumber, lastName, firstName FROM employees WHERE firstName LIKE 'T_m';
+# Dentro de la tabla employees, obten el número de empleado, apellido y nombre de todos los empleados cuyo nombre no inicia con B
+SELECT employeeNumber, lastName, firstName FROM employees WHERE firstNAME NOT LIKE 'B%';
+# Dentro de la tabla products, obten el código de producto y nombre de los productos cuyo código incluye la cadena _20
+SELECT productCode, productName FROM products WHERE productCode LIKE '%_20%';
+# Dentro de la tabla orderdetails, obten el total de cada orden
+SELECT orderNumber, sum(quantityOrdered*priceEach) AS total FROM orderdetails GROUP BY orderNumber;
+# Dentro de la tabla orders obten el número de órdenes por año.
+SELECT EXTRACT(YEAR FROM orderDate) AS yearDate, count(*) AS total FROM orders GROUP BY EXTRACT(YEAR FROM orderDate); 
+# Obten el apellido y nombre de los empleados cuya oficina está ubicada en USA
+SELECT lastName, firstName, officeCode FROM employees WHERE officeCode IN (SELECT officeCode FROM offices WHERE country = 'USA');
+# Obten el número de cliente, número de cheque y cantidad del cliente que ha realizado el pago más alto
+SELECT customerNumber, checkNumber, amount FROM payments WHERE amount = (SELECT MAX(amount) maxAmount FROM payments);
+# Obten el número de cliente, número de cheque y cantidad de aquellos clientes cuyo pago es más alto que el promedio
+SELECT customerNumber, checkNumber, amount FROM payments WHERE amount > (SELECT AVG(amount) maxAmount FROM payments);
+# Obten el nombre de aquellos clientes que no han hecho ninguna orden.
+SELECT customerNumber, customerName FROM customers WHERE customerNumber NOT IN (SELECT customerNumber FROM orders);
+# Obten el máximo, mínimo y promedio del número de productos en las órdenes de venta
+SELECT MAX(quantityOrdered) AS maxOrdered, MIN(quantityOrdered) AS minOrdered, AVG(quantityordered) AS averageOrdered FROM orderdetails;
+# Dentro de la tabla orders, obten el número de órdenes que hay por cada estado
+SELECT status, COUNT(*) FROM orders GROUP BY status ;
+
+
+
+
+
+
+
+
